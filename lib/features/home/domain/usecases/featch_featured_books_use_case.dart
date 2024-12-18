0507fb5cf31fd918553bf4e925b0079880c0d64a
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-12-18 15:59:08 +0200

    refactor use case using generics

--- lib/features/home/domain/usecases/featch_featured_books_use_case.dart
+++ lib/features/home/domain/usecases/featch_featured_books_use_case.dart
@@ -3,11 +3,16 @@ import 'package:bookley_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookley_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FeatchFeaturedBooksUseCase {
+class FeatchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
   FeatchFeaturedBooksUseCase(this.homeRepo);
 
-  Future<Either<Failures, List<BookEntity>>> fetchFeaturedBooks() {
-    return homeRepo.fetchFeaturedBooks();
+  @override
+  Future<Either<Failures, List<BookEntity>>> call([void parameter]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
+}
+
+abstract class UseCase<Type, param> {
+  Future<Either<Failures, Type>> call([param parameter]);
 }
