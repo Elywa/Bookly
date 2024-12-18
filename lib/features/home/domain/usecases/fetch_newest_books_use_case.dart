c6ade83f4e6108baf4236f565ef2257bbc8008bd
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-12-18 16:02:02 +0200

    add fetch newest books use case

--- lib/features/home/domain/usecases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/usecases/fetch_newest_books_use_case.dart
@@ -4,14 +4,12 @@ import 'package:bookley_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookley_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FeatchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
-  FeatchFeaturedBooksUseCase(this.homeRepo);
+  FetchNewestBooksUseCase(this.homeRepo);
 
   @override
   Future<Either<Failures, List<BookEntity>>> call([void parameter]) async {
-    return await homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchNewestBooks();
   }
 }
-
-
