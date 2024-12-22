d5c0114358f44f66f29de059a5499a6f8654dc31
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-12-22 13:30:08 +0200

    implement remote fetch featured books

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -4,3 +4,5 @@ const kprimapryColor = Color(0xff100B20);
 const kfontFamily = 'Zilla Slab';
 const kfontFamily2 = 'Playfair Display';
 const kFontFamily3 = 'GT Sectra Fine';
+const kFetchFeaturedBookEndPoint =
+    'volumes?Filtering=free-ebooks&q=programming';
---
+++ lib/features/home/data/remote data source/home_remote_data_source.dart
@@ -0,0 +1,34 @@
+import 'package:bookley_app/core/utils/api_services.dart';
+import 'package:bookley_app/core/utils/constants.dart';
+import 'package:bookley_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookley_app/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImple extends HomeRemoteDataSource {
+  final ApiServices api;
+  HomeRemoteDataSourceImple({required this.api});
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await api.get(endPoint: kFetchFeaturedBookEndPoint);
+    List<BookEntity> books = fetchBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> fetchBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var book in data['items']) {
+      books.add(BookModel.fromJson(book));
+    }
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
