e8b50a1ea3d94faa447bf225afe0dae8b62f0de0
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-12-24 14:36:27 +0200

    implement featch featured books cubit

--- lib/features/home/presentation/manager/featured books cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured books cubit/featured_books_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookley_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookley_app/features/home/domain/usecases/featch_featured_books_use_case.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final FeatchFeaturedBooksUseCase featchFeaturedBooksUseCase;
+  FeaturedBooksCubit(this.featchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+
+  Future<void> getFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    final result = await featchFeaturedBooksUseCase.call();
+    result.fold((fail) {
+      emit(FeaturedBooksFailure(fail.message));
+    }, (books) {
+      emit(FeaturedBooksSucess(books));
+    });
+  }
 }
