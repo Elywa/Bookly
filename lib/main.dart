2f50eed8ebd81995a9ae9ee26fdf3bf07bf27526
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-12-24 23:26:19 +0200

    provide Newest and Featured books cubits to material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:bookley_app/core/utils/app_routes.dart';
 import 'package:bookley_app/core/utils/constants.dart';
+import 'package:bookley_app/core/utils/setup_service_locator.dart';
+import 'package:bookley_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookley_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookley_app/features/home/domain/usecases/featch_featured_books_use_case.dart';
+import 'package:bookley_app/features/home/domain/usecases/fetch_newest_books_use_case.dart';
 import 'package:bookley_app/features/home/presentation/manager/featured%20books%20cubit/featured_books_cubit.dart';
+import 'package:bookley_app/features/home/presentation/manager/newest%20books%20cubit/fetch_newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,9 +28,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) {
-          return FeaturedBooksCubit(FeatchFeaturedBooksUseCase());
-        })
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              FeatchFeaturedBooksUseCase(
+                locator.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            return FetchNewestBooksCubit(
+              FetchNewestBooksUseCase(
+                locator.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        ),
       ],
       child: MaterialApp.router(
         routerConfig: AppRoutes.router,
